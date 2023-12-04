aa43730c3a28e26e375f16ae00cd3d33566122cc
unknown <it@thinksurfmedia.com>
2023-12-04 14:02:39 +0530

    7th commit

--- script.php
+++ script.php
@@ -2,6 +2,7 @@
 //user:cinecafes
 //Password:derdWKqa0782Z
 //https://github.com/cinecafes/cinecafes.git
-exec("cd /home6/thinksurfmedia/public_html/1callatmservice/");
-exec("git pull https://github.com/1callatmservice/1call.git main");
+//exec("cd /home6/thinksurfmedia/public_html/1callatmservice/");
+//exec("git pull https://github.com/1callatmservice/1call.git main");
+shell_exec("cd /home6/thinksurfmedia/public_html/1callatmservice/" && sudo "git pull https://github.com/1callatmservice/1call.git main");
 ?>
