3eb0e8ca13e8adf230b3bd227081494239f5a135
unknown <it@thinksurfmedia.com>
2023-12-04 12:53:23 +0530

    6th commit

--- script.php
+++ script.php
@@ -2,6 +2,6 @@
 //user:cinecafes
 //Password:derdWKqa0782Z
 //https://github.com/cinecafes/cinecafes.git
-
+exec("cd /home6/thinksurfmedia/public_html/1callatmservice/");
 exec("git pull https://github.com/1callatmservice/1call.git main");
 ?>
